30c096e533e392adc4fc0aff595da23b180ac26a
Mohit || Dev <93594454+mohitdebian@users.noreply.github.com>
2024-01-01 23:50:57 +0530

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.movie-web.app";
+export const DISCORD_LINK = "https://discord.com";
 export const GITHUB_LINK = "https://github.com/mohitdebian/Cinemate-Hub";
 export const DONATION_LINK =
   "https://pay.google.com/payments/u/0/sendmoney?pa=9414693895@fam&pn=Mohit%20Singh";
