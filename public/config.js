32da4964b103f22344a62e2bad4fac93293cdbc5
Moab-labs-Ltd <mohitlabs43@gmail.com>
2024-01-01 14:24:35 +0530

    first commit

---
+++ public/config.js
@@ -0,0 +1,19 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  VITE_CORS_PROXY_URL: "https://simple-proxy.mohitdebian.workers.dev",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNmQyM2ViZDI0Y2NlNTAwMGRhODA1NjkxODBjNTc0NSIsInN1YiI6IjY0ZWNlOWUzNDU4MTk5MDBjNmY5OTk3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IZlZ2SaZYLFSMPcjXNiK1GoaXREp48QqA_4nK9siOyk",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: "",
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: "https://backend.movie-web.app",
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: ""
+};
---
+++ src/setup/constants.ts
@@ -0,0 +1,7 @@
+export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
+export const DISCORD_LINK = "https://discord.movie-web.app";
+export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
+export const DONATION_LINK =
+  "https://pay.google.com/payments/u/0/sendmoney?pa=9414693895@fam&pn=Mohit%20Singh";
+export const GA_ID = "G-44YVXRL61C";
+export const BACKEND_URL = "https://backend.movie-web.app";
