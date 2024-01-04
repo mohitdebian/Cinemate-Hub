e1273e964bfa352b2113a40771ce02c403cc045f
Mohit || Dev <93594454+mohitdebian@users.noreply.github.com>
2024-01-04 20:15:34 +0530

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -2,6 +2,6 @@ export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://discord.com";
 export const GITHUB_LINK = "https://github.com/mohitdebian/Cinemate-Hub";
 export const DONATION_LINK =
-  "https://pay.google.com/payments/u/0/sendmoney?pa=9414693895@fam&pn=Mohit%20Singh";
+  "https://upi-linkpe.netlify.app/index.html?pa=mohitlab@fam&pn=Mohit Singh&cu=INR";
 export const GA_ID = "G-44YVXRL61C";
 export const BACKEND_URL = "https://backend.movie-web.app";
