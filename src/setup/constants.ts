907e3f8ebdee1d2c5455e26c346df4246915b95d
Mohit || Dev <93594454+mohitdebian@users.noreply.github.com>
2024-01-06 01:25:19 +0530

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://discord.com";
-export const GITHUB_LINK = "https://github.com/mohitdebian/Cinemate-Hub";
+export const GITHUB_LINK = "https://github.com/mohitdebian/PeakWatch";
 export const DONATION_LINK =
   "https://upi-linkpe.netlify.app/index.html?pa=mohitlab@fam&pn=Mohit Singh&cu=INR";
 export const GA_ID = "G-44YVXRL61C";
